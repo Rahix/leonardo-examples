78936c38c200ec7803a14ebd58fa240044d777b1
Rahix <rahix@rahix.de>
2018-09-30 20:50:45 +0200

    Optimize for size
    Signed-off-by: Rahix <rahix@rahix.de>

--- mkhex.sh
+++ mkhex.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 avr-objcopy -j .text -j .data -O ihex target/avr-atmega32u4/release/$1.elf target/$1.hex
-echo "$(cat target/$1.hex | wc -c | numfmt --to=si) Bytes used."
+BYTES=$(cat target/$1.hex | wc -c)
+echo "$(numfmt --to=si $BYTES) Bytes used ($BYTES exact)."
